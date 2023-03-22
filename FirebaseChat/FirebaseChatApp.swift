b93a773ba71d37d32cc7a9eafabf9330e405a63f
Rafal Pawelec (PL) <rafal.pawelec@pwc.com>
2023-03-22 18:13:20 +0100

    Initial Commit

---
+++ FirebaseChat/FirebaseChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseChatApp.swift
+//  FirebaseChat
+//
+//  Created by Rafal Pawelec on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
